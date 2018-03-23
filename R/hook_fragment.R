d15d68708177fc2c07d1a6176c86b63fdf13792e
MurakamiKeach <m.keach.11@gmail.com>
2018-03-23 13:27:30 +0900

    docs: added roxygen descriptions

--- R/hook_fragment.R
+++ R/hook_fragment.R
@@ -1,9 +1,30 @@
+#' Set hooks for knitr hooks for revealjs presentation
+#'
+#' @description
+#' Chunk options \code{fragment_style} and \code{fragment_index} are
+#' passed to 'class' and 'fragment-data-index' in html, respectively.
+#' This function may usually be evaluated in the setup chunk.
+#' 
+#' @export
+#' @return this function does not return any object.
+#' @examples
+#' \dontrun{
+#' ```{r fragment_style = "fragment fade-up", fragment_index = 3}
+#' } 
 set_reveal_hooks <-
   function(){
     knitr::knit_hooks$set(fragment_style = hook_fragment_style, fragment_index = hook_fragment_index)
     knitr::opts_chunk$set(fragment_style = "fragment")
   }
 
+#' Knitr hook for styling the fragment appearance
+#' 
+#' @description
+#' This hook is used for adding a 'class' tag to a figure.
+#' This hook is usually used along with \code{hook_fragment_index}.
+#' In order to close <div> tag correctly, \code{hook_fragment_style} must be passed before the \code{hook_fragment_index}.
+#' 
+#' @seealso hook_fragment_index
 hook_fragment_style <-
   function(before, options, envir) {
     if (before) {
@@ -13,6 +34,13 @@ hook_fragment_style <-
     }
   }
 
+#' Knitr hook for arranging order of the evaluation of fragments
+#' 
+#' @description
+#' This hook is used for adding a 'data-fragment-index' tag to a figure.
+#' This hook is usually used along with \code{hook_fragment_style}.
+#' 
+#' @seealso hook_fragment_style
 hook_fragment_index <-
   function(before, options, envir) {
     if (before) {
