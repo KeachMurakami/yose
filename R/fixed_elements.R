f877cabf708213c0a5f11d7634e169d9cefab989
KeachMurakami <m.keach.11@gmail.com>
2018-07-04 12:04:39 +1000

    feat: header

--- R/fixed_elements.R
+++ R/fixed_elements.R
@@ -2,12 +2,25 @@
 #'
 #' @description.
 #' This function is usually used as inline in revealjs_presentation.
-#' For now, the top end of the footer is controled manually by body_height argument.
+#' For now, the top end of the footer is controled manually by body_offset argument.
 #'
 #' @export
 #' @return this function returns <div> tag for the footer.
 start_footer <-
-  function(height = "100px", body_heigth = "600px", color = "#555555", backgroundcolor = "#aaaaaa"){
-    stringr::str_glue("<div style='position:absolute; top:{body_heigth}; left:-50%; width:200%; height:{height}; color:{color}; background-color:{backgroundcolor}'>") %>%
+  function(height = "100px", body_offset = "600px", color = "#555555", backgroundcolor = "#aaaaaa"){
+    stringr::str_glue("<div style='position:absolute; top:{body_offset}; left:-50%; width:200%; height:{height}; color:{color}; background-color:{backgroundcolor}'>") %>%
+      htmltools::HTML()
+  }
+
+#' Insert header field to revealjs slide
+#'
+#' @description.
+#' This function is usually used as inline in revealjs_presentation.
+#'
+#' @export
+#' @return this function returns <div> tag for the header.
+start_header <-
+  function(height = "100px", body_offset = "-50px", color = "#555555", backgroundcolor = "#aaaaaa"){
+    stringr::str_glue("<div style='position:absolute; top:{body_offset}; left:-50%; width:200%; height:{height}; color:{color}; background-color:{backgroundcolor}'>") %>%
       htmltools::HTML()
   }
